7e0f1521a7c093779fbaeb9497418ad288a1431e
Artem Chystiakov <artem.ch31@gmail.com>
2022-04-01 12:34:36 +0300

    added contract logging

--- deploy/runners/logger.js
+++ deploy/runners/logger.js
@@ -2,11 +2,19 @@ function logTransaction(tx, name) {
   console.log(`Transaction ${name}: Gas used ${tx.receipt.gasUsed}, Hash ${tx.tx}\n`);
 }
 
-function logAddress(name, address) {
-  console.log("CONTRACT: " + name + " ----- " + address + "\n");
+function logContracts(...contracts) {
+  let table = [];
+
+  for (let i = 0; i < contracts.length; i++) {
+    table.push({ "Proxy Contract": contracts[i][0], Address: contracts[i][1] });
+  }
+
+  console.table(table);
+
+  console.log();
 }
 
 module.exports = {
-  logAddress,
   logTransaction,
+  logContracts,
 };
