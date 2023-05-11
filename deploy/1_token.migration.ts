233f6d9a10a3572da88b0826a30418434a91b7d6
dovgopoly <idovgopoly@gmail.com>
2023-05-11 19:15:12 +0300

    fixes

--- deploy/1_token.migration.ts
+++ deploy/1_token.migration.ts
@@ -4,7 +4,7 @@ import { artifacts } from "hardhat";
 const ERC20Mock = artifacts.require("ERC20Mock");
 
 export = async (deployer: Deployer, logger: Logger) => {
-  const erc20 = await deployer.deploy(ERC20Mock);
+  const erc20 = await deployer.deploy(ERC20Mock, "Mock", "Mock", 18);
 
   logger.logContracts(["ERC20Mock", erc20.address]);
 };
