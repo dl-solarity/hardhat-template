6aec4b5c14560bcb245893073c1a7132857e9d60
dovgopoly <idovgopoly@gmail.com>
2023-05-12 14:20:33 +0300

    reordered util methods

--- scripts/utils/utils.ts
+++ scripts/utils/utils.ts
@@ -2,6 +2,14 @@ import { ethers } from "hardhat";
 import { BigNumber } from "bignumber.js";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
+export function toBN(value: string | number | BigNumber | bigint) {
+  if (typeof value == "bigint") {
+    value = value.toString();
+  }
+
+  return new BigNumber(value);
+}
+
 export function wei(value: string | number | bigint, decimal: number = 18): bigint {
   if (typeof value == "number" || typeof value == "bigint") {
     value = value.toString();
@@ -18,18 +26,10 @@ export function weiUSDT(value: string | number | bigint): bigint {
   return wei(value, 6);
 }
 
-export async function account(index: number): Promise<SignerWithAddress> {
-  return (await ethers.getSigners())[index];
-}
-
-export function toBN(value: string | number | BigNumber | bigint) {
-  if (typeof value == "bigint") {
-    value = value.toString();
-  }
-
-  return new BigNumber(value);
-}
-
 export function fromWei(value: string | number | bigint, decimal: number = 18): string {
   return toBN(value).div(toBN(10).pow(decimal)).toString();
+}
+
+export async function accounts(index: number): Promise<SignerWithAddress> {
+  return (await ethers.getSigners())[index];
 }
