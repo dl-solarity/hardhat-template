ed9e565bfb33639e4105a8ad6778356c603e9097
aritkulova <aritkulova.yuliia@gmail.com>
2023-11-29 17:24:33 +0200

    packages updated
    added bignumber.js
    hardhat-migrate to 2.0.0
    openzeppelin to 5.0.0
    migration updated

--- deploy/1_token.migration.ts
+++ deploy/1_token.migration.ts
@@ -1,10 +1,10 @@
-import { Deployer, Logger } from "@solarity/hardhat-migrate";
+import { Deployer, Reporter } from "@solarity/hardhat-migrate";
 import { artifacts } from "hardhat";
 
 const ERC20Mock = artifacts.require("ERC20Mock");
 
-export = async (deployer: Deployer, logger: Logger) => {
-  const erc20 = await deployer.deploy(ERC20Mock, "Mock", "Mock", 18);
+export = async (deployer: Deployer) => {
+  const erc20 = await deployer.deploy(ERC20Mock, ["Mock", "Mock", 18]);
 
-  logger.logContracts(["ERC20Mock", erc20.address]);
+  Reporter.reportContracts(["ERC20Mock", erc20.address]);
 };
