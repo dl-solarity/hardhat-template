2773a3f7865a77b064fcf42a8679d22218dbb5cc
Yuliia Aritkulova <94910987+aritkulova@users.noreply.github.com>
2023-12-01 15:43:43 +0200

    Update packages (#11)
    * Updated migrations
    
    * Added bignumber.js
    
    * Updated hardhat-migrate to 2.0.0
    
    * Updated OpenZeppelin to 5.0.0
    
    * Updated dependencies
    
    ---------
    
    Co-authored-by: Artem Chystiakov <artem.ch31@gmail.com>

--- deploy/1_token.migration.ts
+++ deploy/1_token.migration.ts
@@ -1,10 +1,9 @@
-import { Deployer, Logger } from "@solarity/hardhat-migrate";
-import { artifacts } from "hardhat";
+import { Deployer, Reporter } from "@solarity/hardhat-migrate";
 
-const ERC20Mock = artifacts.require("ERC20Mock");
+import { ERC20Mock__factory } from "@ethers-v6";
 
-export = async (deployer: Deployer, logger: Logger) => {
-  const erc20 = await deployer.deploy(ERC20Mock, "Mock", "Mock", 18);
+export = async (deployer: Deployer) => {
+  const erc20 = await deployer.deploy(ERC20Mock__factory, ["Mock", "Mock", 18]);
 
-  logger.logContracts(["ERC20Mock", erc20.address]);
+  Reporter.reportContracts(["ERC20Mock", await erc20.getAddress()]);
 };
